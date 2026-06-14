84f4548835bebd670655a573e11144a4cc16e69e
kimdoyun98 <rladlsxo1250@gmail.com>
2026-06-14 17:39:49 +0900

    feat: Router 설정 #2

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:ddara/core/router/route_path.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../feature/home/home_page.dart';
+import '../../feature/login/login_page.dart';
+
+final initialRouteProvider = Provider<String>((ref) => RoutePath.login);
+
+final routerProvider = Provider<GoRouter>((ref) {
+  final initialRoute = ref.read(initialRouteProvider);
+
+  return GoRouter(
+    initialLocation: initialRoute,
+    routes: [
+      GoRoute(path: RoutePath.login, builder: (_, _) => const LoginPage()),
+      GoRoute(path: RoutePath.home, builder: (_, _) => const HomePage()),
+    ],
+  );
+});
