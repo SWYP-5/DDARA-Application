37f1d2c0ef15e73e28558d9efdf688317d9ec884
kimdoyun98 <rladlsxo1250@gmail.com>
2026-06-22 11:45:52 +0900

    feat: Firebase 앱 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'core/router/app_router.dart';
 import 'core/router/route_path.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     ProviderScope(
