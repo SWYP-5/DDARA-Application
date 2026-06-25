22b5fd5b388a68e65c22188169230bf6624aec67
kimdoyun98 <rladlsxo1250@gmail.com>
2026-06-25 13:04:03 +0900

    chore: 폴더명 변경

---
+++ lib/feature/group/started/started_camera_page.dart
@@ -0,0 +1,21 @@
+import 'package:ddara/core/designsystem/component/appbar/app_bar.dart';
+import 'package:ddara/core/widget/camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+
+class StartedCameraPage extends StatelessWidget {
+  const StartedCameraPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const AppBar(),
+      child: Camera(
+        // TODO: 모임 상태에 따라 투명도/모드 영역 표시 여부 결정.
+        showOpacity: true,
+        showViewMode: true,
+        // TODO: 친구가 미리 찍은 사진으로 교체. (현재 테스트용 임시 이미지)
+        guideImage: const AssetImage('assets/images/temp_image.jpg'),
+      ),
+    );
+  }
+}
