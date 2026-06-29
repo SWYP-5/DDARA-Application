5f8b946602b5b3a4dd55613b136cbbe78bbb5804
kimdoyun98 <rladlsxo1250@gmail.com>
2026-06-29 20:08:28 +0900

    feat: 이름은 소셜 이름(닉네임)으로 적용

--- lib/domain/usecase/auth/signup_use_case.dart
+++ lib/domain/usecase/auth/signup_use_case.dart
@@ -15,30 +15,33 @@ class SignUpUseCase {
     this._googleAuthService,
   );
 
-  Future<void> call(
-    SocialLoginType type,
-    bool termsAgreed,
-    String birthDate,
-    String nickName,
-  ) async {
-    final token = switch (type) {
-      SocialLoginType.kakao => await _kakaoAuthService.getKakaoAccessToken(),
-      SocialLoginType.google => await _googleAuthService.getGoogleAccessToken(),
+  Future<void> call(SocialLoginType type, bool termsAgreed) async {
+    // 닉네임 입력 화면을 제거하고, 소셜 로그인 프로필 이름을 닉네임으로 사용한다.
+    final (token, name) = switch (type) {
+      SocialLoginType.kakao => (
+        await _kakaoAuthService.getKakaoAccessToken(),
+        await _kakaoAuthService.getKakaoName(),
+      ),
+      SocialLoginType.google => (
+        await _googleAuthService.getGoogleAccessToken(),
+        _googleAuthService.getGoogleName(),
+      ),
     };
 
-    final response = await _authRepository.signUp(
-      SignUpCommand(
-        provider: type.value,
-        accessToken: token!,
-        birthDate: birthDate,
-        termsAgreed: termsAgreed,
-        nickname: nickName,
-      ),
-    );
+    // final response = await _authRepository.signUp(
+    //   SignUpCommand(
+    //     provider: type.value,
+    //     accessToken: token!,
+    //     // 생일 입력 화면 제거. API 에서 birthDate 필드가 빠지면 함께 정리한다.
+    //     birthDate: '',
+    //     termsAgreed: termsAgreed,
+    //     nickname: name ?? '',
+    //   ),
+    // );
 
-    await _authRepository.saveAccessToken(response.accessToken);
-    await _authRepository.saveRefreshToken(response.refreshToken);
-    // 토큰 만료 시 무중단 재인증에서 분기하도록 소셜 종류도 저장.
-    await _authRepository.saveSocialLoginType(type);
+    // await _authRepository.saveAccessToken(response.accessToken);
+    // await _authRepository.saveRefreshToken(response.refreshToken);
+    // // 토큰 만료 시 무중단 재인증에서 분기하도록 소셜 종류도 저장.
+    // await _authRepository.saveSocialLoginType(type);
   }
 }
